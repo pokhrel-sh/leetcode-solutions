c704a313b5a783d6201b1882b70a834d8ea71148
Shishir Pokhrel <pokhrel.sh@northeastern.edu>
2024-04-16 15:41:31 -0700

    add sortColor 75

---
+++ sortColor75.java
@@ -0,0 +1,10 @@
+// bad answer
+
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);  // do it again, but real this time.
+    }
+}
+
+
+//good answer will be given soon!.
